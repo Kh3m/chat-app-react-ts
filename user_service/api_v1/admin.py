4a69a014fe1788b44f0c71b01c2acf7940c0f06c
Chigozirim <iamokwukwe@gmail.com>
2023-10-04 17:03:20 +0100

    Register Models in admin.

--- user_service/api_v1/admin.py
+++ user_service/api_v1/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from api_v1.models import User, Address, Profile
 
 # Register your models here.
+admin.site.register(Address)
+admin.site.register(Profile)
+admin.site.register(User, UserAdmin)
