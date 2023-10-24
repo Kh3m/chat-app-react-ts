33909cb63fe6d4ba27f8413b75d28786107a49ca
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2023-10-24 21:41:14 +0100

    Prefix all endpoints with /users

--- user_service/api_v1/urls.py
+++ user_service/api_v1/urls.py
@@ -9,17 +9,17 @@ from api_v1.views import GoogleLogin, Generate_user_cert
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename="user")
-router.register(r'profiles', ProfileViewSet, basename='profile')
-router.register(r'Adresses', AddressViewSet, basename="address")
+router.register(r'users/profiles', ProfileViewSet, basename='profile')
+router.register(r'users/adresses', AddressViewSet, basename="address")
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('auth/', include('dj_rest_auth.urls')),
-    path('auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('users/auth/', include('dj_rest_auth.urls')),
+    path('users/auth/registration/', include('dj_rest_auth.registration.urls')),
     path("signup/", signup, name="socialaccount_signup"),
-    path('auth/google/', GoogleLogin.as_view(), name='google_login'),
-    path('auth/cert/', Generate_user_cert.as_view(), name='user_cert'),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api-docs/',
+    path('users/auth/google/', GoogleLogin.as_view(), name='google_login'),
+    path('users/auth/cert/', Generate_user_cert.as_view(), name='user_cert'),
+    path('users/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('users/api-docs/',
          SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', include(router.urls)),
 ]
