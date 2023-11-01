8fad2b4b49f47dc8e72f8ea2950d1628a82530bb
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2023-11-01 12:09:18 +0100

    Customize allauth Account Adapter to publish events instead of send emails

---
+++ user_service/api_v1/adapters.py
@@ -0,0 +1,49 @@
+"""
+Defines a customized Account Adapter for allauth
+https://github.com/pennersr/django-allauth/blob/main/allauth/account/adapter.py#L174
+"""
+
+from allauth.account.adapter import DefaultAccountAdapter
+
+from api_v1.events import publishers
+
+
+class CustomAccountAdapter(DefaultAccountAdapter):
+    def send_mail(self, template_prefix, email, context):
+        """
+        The default behavior of this function is to send an email.
+        It is now overridden to publish an event instead.
+
+        Instead of sending an email, this function publishes an event based on
+        the context data provided. The specific event published depends on the
+        content of the 'context' dictionary.
+
+        If the 'context' dictionary includes an 'activate_url', an event related
+            to a new registration is published, providing information about the
+            user and the activation key.
+
+        If the 'context' dictionary includes a 'password_reset_url', an event
+            related to a password reset is published. This event contains the
+            reset token and user ID.
+
+        For other cases, where neither activation nor password reset is
+        detected, the function sends the message using the default email
+        sending mechanism.
+
+        """
+        if 'activate_url' in context:
+            publishers.publish_new_registration(
+                context['user'], context['key'])
+            print(context.get('activate_url'))
+
+        elif 'password_reset_url' in context:
+            reset_url_parts = context.get('password_reset_url').split('/')
+            if reset_url_parts[-1] == '':
+                reset_url_parts.pop()
+
+            token, uidb64 = reset_url_parts[-1], reset_url_parts[-2]
+            publishers.publish_password_reset(token, uidb64)
+
+        else:
+            msg = self.render_mail(template_prefix, email, context)
+            msg.send()
