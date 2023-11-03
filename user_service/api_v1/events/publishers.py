27eb138fba9bdefd946cc029e4bbe197bfddd70a
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2023-11-03 14:03:00 +0100

    Write new publishers

--- user_service/api_v1/events/publishers.py
+++ user_service/api_v1/events/publishers.py
@@ -1,34 +1,44 @@
-import pika
-import json
-from pika.exchange_type import ExchangeType
+import logging
+from .utils.rbmq import RBMQ
+
+logger = logging.getLogger("api_v1_events")
 
 # Establish a connection to the RabbitMQ server
-connection_parameters = pika.ConnectionParameters('localhost')
-connection = pika.BlockingConnection(connection_parameters)
-channel = connection.channel()
-
-# Declare an exchange for user-related events
-channel.exchange_declare(
-    exchange='user_service_exchange', exchange_type=ExchangeType.direct)
+rbmq_client = RBMQ(exchange_name="user_service_exchange",
+                   exchange_type="direct")
 
 
-def publish_new_registration(user_data, key):
-    message = {"user": user_data, "key": key}
-    channel.basic_publish(
-        exchange="user_service_exchange",
-        routing_key="new_registration",
-        body=json.dumps(message)
-    )
+def publish_user_signed_up(user_data):
+    routing_key = "user_signed_up"
+    message = {"user": user_data}
+    rbmq_client.publish_event(message, routing_key)
 
 
-def publish_password_reset(token, uidb64):
-    message = {"token": token, "uidb64": uidb64}
-    channel.basic_publish(
-        exchange="user_service_exchange",
-        routing_key="password_reset",
-        body=json.dumps(message)
-    )
+def publish_user_logged_in(user_data):
+    routing_key = "user_logged_in"
+    message = {"user": user_data}
+    rbmq_client.publish_event(message, routing_key)
 
 
-def publish_user_logged_out():
-    pass
+def publish_user_logged_out(user_id, auth_token):
+    routing_key = "user_logged_out"
+    message = {"user_id": user_id, "auth_token": auth_token}
+    rbmq_client.publish_event(message, routing_key)
+
+
+def publish_email_confirmation(email, key):
+    routing_key = "email_confirmation"
+    message = {"email": email, "key": key}
+    rbmq_client.publish_event(message, routing_key)
+
+
+def publish_email_confirmed(email):
+    routing_key = "email_confirmed"
+    message = {"email": email}
+    rbmq_client.publish_event(message, routing_key)
+
+
+def publish_password_reset(email, token, uidb64):
+    routing_key = "password_reset"
+    message = {"user_email": email, "token": token, "uidb64": uidb64}
+    rbmq_client.publish_event(message, routing_key)
