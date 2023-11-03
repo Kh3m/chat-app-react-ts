37ad618eaea2b2fb977c0970f1442280c7dcf9ce
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2023-11-03 14:03:39 +0100

    Update

--- user_service/api_v1/urls.py
+++ user_service/api_v1/urls.py
@@ -12,7 +12,7 @@ from dj_rest_auth.views import (
     PasswordResetView,
     LoginView,
     LogoutView,
-    UserDetailsView
+    UserDetailsView,
 )
 
 from api_v1.views import UserViewSet, ProfileViewSet, AddressViewSet
@@ -25,31 +25,49 @@ router.register(r'profiles', ProfileViewSet, basename='profile')
 router.register(r'adresses', AddressViewSet, basename="address")
 router.register(r'', UserViewSet, basename="user")
 
+# urlpatterns = [
+#     path("auth/user/", UserDetailsView.as_view(), name="rest_user_details"),
+#     path("auth/register/", RegisterView.as_view(), name="rest_register"),
+#     path("auth/register/verify-email/", VerifyEmailView.as_view(),
+#          name="rest_verify_email"),
+#     path("auth/register/resend-email/", ResendEmailVerificationView.as_view(),
+#          name="rest_resend_email"),
+#     path("auth/login/", LoginView.as_view(), name="rest_login"),
+#     path("auth/logout/", LogoutView.as_view(), name="rest_logout"),
+#     path("auth/account-confirm-email/<str:key>/",
+#          email_confirm_redirect, name="account_confirm_email"),
+#     path("auth/account-confirm-email/", VerifyEmailView.as_view(),
+#          name="account_email_verification_sent"),
+#     path("auth/password/reset/", PasswordResetView.as_view(),
+#          name="rest_password_reset"),
+#     path(
+#         "auth/password/reset/confirm/<str:uidb64>/<str:token>/",
+#         password_reset_confirm_redirect,
+#         name="password_reset_confirm",
+#     ),
+#     path("auth/password/reset/confirm/", PasswordResetConfirmView.as_view(),
+#          name="password_reset_confirm"),
+
+#     path('auth/google/', GoogleLogin.as_view(), name='google_login'),
+#     path('auth/cert/', Generate_user_cert.as_view(), name='user_cert'),
+#     path('schema/', SpectacularAPIView.as_view(), name='schema'),
+#     path('api-docs/',
+#          SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+#     path('', include(router.urls)),
+# ]
+
 urlpatterns = [
-    path("auth/user/", UserDetailsView.as_view(), name="rest_user_details"),
-    path("auth/register/", RegisterView.as_view(), name="rest_register"),
-    path("auth/register/verify-email/", VerifyEmailView.as_view(),
-         name="rest_verify_email"),
-    path("auth/register/resend-email/", ResendEmailVerificationView.as_view(),
-         name="rest_resend_email"),
-    path("auth/login/", LoginView.as_view(), name="rest_login"),
-    path("auth/logout/", LogoutView.as_view(), name="rest_logout"),
-    path("auth/account-confirm-email/<str:key>/",
-         email_confirm_redirect, name="account_confirm_email"),
-    path("auth/account-confirm-email/", VerifyEmailView.as_view(),
-         name="account_email_verification_sent"),
-    path("auth/password/reset/", PasswordResetView.as_view(),
-         name="rest_password_reset"),
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('auth/google/', GoogleLogin.as_view(), name='google_login'),
+    path('auth/cert/', Generate_user_cert.as_view(), name='user_cert'),
     path(
         "auth/password/reset/confirm/<str:uidb64>/<str:token>/",
         password_reset_confirm_redirect,
         name="password_reset_confirm",
     ),
-    path("auth/password/reset/confirm/", PasswordResetConfirmView.as_view(),
-         name="password_reset_confirm"),
-
-    path('auth/google/', GoogleLogin.as_view(), name='google_login'),
-    path('auth/cert/', Generate_user_cert.as_view(), name='user_cert'),
+    path("auth/account-confirm-email/<str:key>/",
+         email_confirm_redirect, name="account_confirm_email"),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api-docs/',
          SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
