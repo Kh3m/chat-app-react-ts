0c0ba9238d43bbd71ecd70dca5a3936563a79106
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2023-11-06 10:04:58 +0100

    Implement ability to CRUD Social and Member models from Store viewset

--- store_service/api_v1/urls.py
+++ store_service/api_v1/urls.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.urls import path, re_path, include
 from rest_framework.routers import DefaultRouter
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -5,13 +6,11 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from api_v1.views import StoreViewSet, MemberViewSet, SocialViewSet
 
 router = DefaultRouter()
-router.register(r'stores', StoreViewSet, basename='store')
-router.register(r'members', MemberViewSet, basename='member')
-router.register(r'socials', SocialViewSet, basename='social')
+router.register(r'', StoreViewSet, basename='store')
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api-docs/',
          SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', include(router.urls)),
 ]
