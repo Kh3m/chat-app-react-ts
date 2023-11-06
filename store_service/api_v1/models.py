12d9526a63fd965edd43796ed54e7db715ae9281
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2023-11-06 10:02:19 +0100

    Change Social url attr to link

--- store_service/api_v1/models.py
+++ store_service/api_v1/models.py
@@ -19,20 +19,32 @@ class Store(models.Model):
     business_license_number = models.CharField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    members = models.ManyToManyField("Member", related_name='store_members', null=True)
-    socials = models.ManyToManyField("Social", related_name='store_socials', null=True)
+    members = models.ManyToManyField(
+        "Member", related_name='store_members', null=True)
+    socials = models.ManyToManyField(
+        "Social", related_name='store_socials', null=True)
 
 
 class Member(models.Model):
     """Manage store member"""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     store = models.ForeignKey(Store, on_delete=models.CASCADE)
-    user = models.UUIDField()
+    user = models.UUIDField(unique=True)
     added_at = models.DateTimeField(auto_now_add=True)
 
 
 class Social(models.Model):
     """Manage store social media"""
+    CHOICES = [
+        ('Twitter', 'Twitter'),
+        ('Facebook', 'Facebook'),
+        ('YouTube', 'YouTube'),
+        ('LinkedIn', 'LinkedIn'),
+        ('Instagram', 'Instagram'),
+        ('WhatsApp', 'WhatsApp')
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     store = models.ForeignKey(Store, on_delete=models.CASCADE)
-    platform = models.CharField(max_length=30)
-    url = models.CharField(max_length=60)
+    platform = models.CharField(max_length=60, choices=CHOICES)
+    link = models.CharField(max_length=60)
