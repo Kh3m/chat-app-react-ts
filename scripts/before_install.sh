0d631093eff075088cdf36cea7285db29146288c
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2023-11-09 14:58:44 +0100

    Install pip for Python 3.12

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -24,18 +24,21 @@ sudo apt-get install -y docker.io
 sudo docker run -d --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
 sudo docker start rabbitmq
 
-# Install the latest Python
+# Install the latest Python and instal pip
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get install python3.12
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.12 2
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 2
 
+# Install pip for Python 3.12
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+
 # Install PostgreSQL
 sudo apt-get install -y postgresql postgresql-contrib
 
 # Install Django
-sudo apt-get install -y python3-pip
 pip3 install Django
 
 # Install Redis
