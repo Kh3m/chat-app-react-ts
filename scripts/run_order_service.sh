f78471d3554f696280255b698434db5787bbe9d0
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2023-11-09 12:48:29 +0100

    Create run_order_service.sh

---
+++ scripts/run_order_service.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+cd /home/ubuntu/django-aws_cicd/order_service/
+
+# Install any software that's unique to the service
+
+# activate virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Set environmental variables
+
+# Install requirements.txt
+pip3 install -r requirements.txt
+
+# run server
+python3 manage.py makemigrations api_v1
+python3 manage.py migrate
+screen -d -m python3 manage.py runserver 0:8002
