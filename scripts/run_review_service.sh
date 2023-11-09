3594e75420b1ca9d95ee140047ccdb277ab24349
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2023-11-09 13:14:11 +0100

    Update run_review_service.sh

--- scripts/run_review_service.sh
+++ scripts/run_review_service.sh
@@ -9,6 +9,15 @@ python3 -m venv venv
 source venv/bin/activate
 
 # Set environmental variables
+DEV_DB_NAME="review_service_dev"
+DEV_DB_USER="fixam_dev"
+DEV_DB_USER_PWD="fixam_dev_pwd"
+
+# Create the PostgreSQL database
+sudo -u postgres psql -c "CREATE DATABASE $DEV_DB_NAME;"
+
+# Grant all privileges to the user on the database
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DEV_DB_NAME TO $DEV_DB_USER;"
 
 # Install requirements.txt
 pip3 install -r requirements.txt
