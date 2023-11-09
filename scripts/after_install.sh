74e3144331f7dd729ab09def8069ff4f6658ebf6
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2023-11-09 13:42:09 +0100

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,4 +6,11 @@ sudo pkill -f runserver
 # kill frontend servers too
 
 # Create the PostgreSQL user
-sudo -u postgres psql -c "CREATE USER fixam_dev WITH ENCRYPTED PASSWORD 'fixam_dev_pwd';"
+# Check if the user exists
+if sudo -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='fixam_dev'" | grep -qw 1; then
+  echo "User fixam_dev already exists."
+else
+  # Create the PostgreSQL user
+  sudo -u postgres psql -c "CREATE USER fixam_dev WITH ENCRYPTED PASSWORD 'fixam_dev_pwd';"
+  echo "User fixam_dev created."
+fi
