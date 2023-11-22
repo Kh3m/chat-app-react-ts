d8b1a2346e7b9bdcb8340f19afa5884d6c503763
Abdul Kareem <khem6333@gmail.com>
2023-11-22 13:42:46 +0100

    Add full file

--- fixam-frontend/src/App.tsx
+++ fixam-frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import linkedIn from "./assets/linkedin.svg";
+import Chat from "./chat/Chat";
+import ChatFixam from "./chat/Chat-Fixam";
 import Banner from "./components/Banner";
 import Button from "./components/Button";
 import Categories from "./components/Categories/Categories";
@@ -12,24 +14,28 @@ import SearchInput from "./components/SearchInput/SearchInput";
 
 function App() {
   return (
-    <main className="md:w-[1140px] m-auto">
-      <div className="flex space-x-1 items-center flex-wrap">
-        <Icon image={{ src: linkedIn, alt: "Fixam LinkedIn Profile" }} />
-        <Logo />
-        <Button>Buy Now</Button>
-        <Button variant="w-icon">
-          <IconPlus />
-          <span>Fix Ad Here</span>
-        </Button>
-        <Button variant="outlined">Register</Button>
+    <main>
+      {/* <Chat /> */}
+      <ChatFixam />
+      <div className="md:w-[1140px] m-auto">
+        <div className="flex space-x-1 items-center flex-wrap">
+          <Icon image={{ src: linkedIn, alt: "Fixam LinkedIn Profile" }} />
+          <Logo />
+          <Button>Buy Now</Button>
+          <Button variant="w-icon">
+            <IconPlus />
+            <span>Fix Ad Here</span>
+          </Button>
+          <Button variant="outlined">Register</Button>
+        </div>
+        <Banner color="yellow" rounded>
+          <SearchInput />
+        </Banner>
+        <Categories />
+        <FeaturedBar />
+        <Products />
+        <NewsLetter />
       </div>
-      <Banner color="yellow" rounded>
-        <SearchInput />
-      </Banner>
-      <Categories />
-      <FeaturedBar />
-      <Products />
-      <NewsLetter />
     </main>
   );
 }
